0e2591ed158a51df1d710eed539d07aa4c974958
Anshuman Sinha <anshumansinha2001@gmail.com>
2024-07-26 11:23:09 +0530

    Applied aggregation pipelines on userController

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -3,6 +3,7 @@ dotenv.config();
 
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
+import path from "path";
 
 // Configuration
 cloudinary.config({
@@ -10,6 +11,22 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+// Helper function to extract public ID from URL
+const getPublicIdFromUrl = (url) => {
+  const urlObj = new URL(url);
+  const pathname = urlObj.pathname;
+  const publicIdWithExtension = pathname
+    .substring(1)
+    .split("/")
+    .slice(1)
+    .join("/");
+  const publicId = publicIdWithExtension.substring(
+    0,
+    publicIdWithExtension.lastIndexOf(".")
+  );
+  return publicId;
+};
 
 // Upload an image
 export const uploadOnCloudinary = async (localFilePath) => {
@@ -20,8 +37,6 @@ export const uploadOnCloudinary = async (localFilePath) => {
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
-
-    // File has been uploaded successfully
     // console.log("file is uploaded on cloudinary ", response.url);
 
     // Remove the locally saved temporary file
@@ -38,5 +53,32 @@ export const uploadOnCloudinary = async (localFilePath) => {
     }
 
     return { error: error.message || "An error occurred during the upload" };
+  }
+};
+
+// Delete an image by URL
+export const deleteFromCloudinary = async (url) => {
+  if (!url) return null;
+
+  try {
+    // Extract the public ID from the URL
+    const publicId = getPublicIdFromUrl(url);
+
+    // Delete the file from Cloudinary
+    const response = await cloudinary.uploader.destroy(publicId);
+
+    // Check the response for the result
+    if (response.result === "ok") {
+      // console.log("file is deleted from cloudinary ", publicId);
+      return { message: "File deleted successfully" };
+    } else {
+      // File deletion failed
+      return { error: "Failed to delete the file" };
+    }
+  } catch (error) {
+    // Log the error
+    console.error("Error deleting from Cloudinary:", error);
+
+    return { error: error.message || "An error occurred during the deletion" };
   }
 };
